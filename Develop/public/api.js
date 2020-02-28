b15e20d2748b56db124cff248fd25ff927dffd93
Louis Alonzo <louis.a@live.com>
2020-02-27 16:39:48 -0800

    modified the routes to proper manage data

--- Develop/public/api.js
+++ Develop/public/api.js
@@ -1,8 +1,10 @@
 const API = {
   async getLastWorkout() {
+    // retrieves array of workouts
     const res = await fetch("/api/workouts");
     const json = await res.json();
 
+    // provides the last workout 
     return json[json.length - 1];
   },
   async addExercise(data) {
@@ -18,10 +20,12 @@ const API = {
 
     return json;
   },
-  async createWorkout() {
+  async createWorkout(data = {}) {
     const res = await fetch("/api/workouts", {
       method: "POST",
-      headers: { "Content-Type": "application/json" }
+      headers: { "Content-Type": "application/json" },
+      // the passing of data as an argument to be value of body
+      body: JSON.stringify(data)
     });
 
     const json = await res.json();
