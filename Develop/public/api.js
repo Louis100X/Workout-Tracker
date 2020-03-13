c426acd70ad2d479462016b4f36ee0f2444d8ed0
Louis Alonzo <louis.a@live.com>
2020-03-13 11:46:41 -0700

    made corrections to API

--- Develop/public/api.js
+++ Develop/public/api.js
@@ -1,5 +1,5 @@
 const API = {
-  async getLastWorkout() {
+  async lastWorkoutadded() {
     // retrieves array of workouts
     const res = await fetch("/api/workouts");
     const json = await res.json();
@@ -7,7 +7,7 @@ const API = {
     // provides the last workout 
     return json[json.length - 1];
   },
-  async addExercise(data) {
+  async newExercise(data) {
     const id = location.search.split("=")[1];
 
     const res = await fetch("/api/workouts/" + id, {
@@ -20,7 +20,7 @@ const API = {
 
     return json;
   },
-  async createWorkout(data = {}) {
+  async newWorkout(data = {}) {
     const res = await fetch("/api/workouts", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -33,7 +33,7 @@ const API = {
     return json;
   },
 
-  async getWorkoutsInRange() {
+  async workoutsRange() {
     const res = await fetch(`/api/workouts/range`);
     const json = await res.json();
     
